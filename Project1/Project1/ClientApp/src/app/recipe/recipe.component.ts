021db265697bf5e9f65b403647e993175e79d86c
jonplumb89 <jonplumb89@outlook.com>
2021-11-12 17:08:39 -0500

    finished toggle menu, messed around with home page, and just about finished off the recipe search page

--- Project1/Project1/ClientApp/src/app/recipe/recipe.component.ts
+++ Project1/Project1/ClientApp/src/app/recipe/recipe.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, Input, OnInit } from '@angular/core';
 import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 import { RecipesService } from '../../recipes.service';
 import { FavoritesService } from '../favorites.service';
 import { Favorites } from '../Models/Favorites';
 import { Recipe, SedIngredient } from '../Models/Recipe';
 import { RecipeInfo } from '../Models/RecipeInfo';
+import { NavMenuComponent } from '../nav-menu/nav-menu.component';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.css']
 })
+
 export class RecipeComponent implements OnInit {
   sourceUrl: string = "";
   recipes: Recipe[] = null;
@@ -21,17 +23,25 @@ export class RecipeComponent implements OnInit {
   in_food: any;
   sAmount: string = '';
   doit: string;
+  sizing: NavMenuComponent;
+
+  //@Input() closeHerUp = '';
+
+  //@HostListener()()
+
 
   get sanSourceUrl() {
     return this.sanatizer.bypassSecurityTrustResourceUrl(this.sourceUrl);
   }
 
   constructor(private recipeService: RecipesService, private sanatizer: DomSanitizer, private favoritesService: FavoritesService) {
-    this.recipeService = recipeService
+    this.recipeService = recipeService;
   }
 
   ngOnInit() {
+    (<HTMLInputElement>document.getElementById("toggleBtn")).checked = false;
   }
+
 
   removeCh(str) {
     if ((str === null) || (str === ''))
@@ -46,13 +56,14 @@ export class RecipeComponent implements OnInit {
   bang(food: string) {
     if (food.length == 0) return;
     this.foods.push(food);
+    this.in_food = '';
   }
 
 
   submitList() {
     let meow = this.foods.join(",");
     console.log(this.sAmount);
-    let url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=6d824812b91b45929944784ade2c9453&number=" + this.sAmount;
+    let url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=3ad1ee3c2ad5442fbe2e4a7b72183b78&number=" + this.sAmount;
     meow = "&ingredients=" + meow;
     this.recipeService.getRecipies(url + meow).subscribe(data => {
       this.recipes = data;
@@ -60,17 +71,27 @@ export class RecipeComponent implements OnInit {
     })
   }
 
-
-  getRecipe(id: number
-  ) {
+  getRecipe(id: number) {
     this.recipeService.getRecipe(id).subscribe(data => {
       this.recipeInfos = data;
       this.extendIngreds = data.extendedIngredients;
       if (this.recipeInfos.instructions != null) {
-        this.doit = this.removeCh(this.recipeInfos.instructions)
+        this.doit = this.removeCh(this.recipeInfos.instructions);
       }
+
       console.log(this.recipeInfos);
+
+      // update and display modal
+      let x = (<ElementCSSInlineStyle>document.getElementById("myModal"));
+      if (x != null) x.style.display = 'block';
     })
+  }
+
+  modalMode($event) {
+    if ($event.target.classList.contains("modalClose")) {
+      let modal = (<ElementCSSInlineStyle>document.getElementById("myModal"));
+      if (modal != null) modal.style.display = "none";
+    }
   }
 
   delete(value: string) {
@@ -84,7 +105,4 @@ export class RecipeComponent implements OnInit {
     console.log(favorite.extendedIngredients)
     this.favoritesService.postMyFavoriteRecipe(favorite).subscribe()
   }
-
-
-
 }
